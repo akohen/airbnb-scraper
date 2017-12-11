79c8c9e112b7b03e59a8e55eb943c8a945079c32
Alexandre Kohen <alexandre@kohen.fr>
2017-12-11 19:05:56 +0000

    Base scraper + tester script

---
+++ index.js
@@ -0,0 +1,21 @@
+const request = require('request')
+    , cheerio = require('cheerio')
+    , scraper = require('./scraper')
+
+const idsToScrape = process.argv.slice(2)
+
+const results = idsToScrape.map( id => {
+  return scraper.scrape(id)
+    .then(property => {
+      return property
+    })
+    .catch(err => {
+      console.log(`Error while scraping ${id}: ${err}`)
+    })
+})
+
+Promise.all(results).then(properties => {
+  properties
+    .filter(i => i) // removing empty responses (if an error was catched)
+    .map(property => console.log(property.name)) // do something with the remaining results
+})
---
+++ scraper.js
@@ -0,0 +1,57 @@
+const request = require('request-promise')
+    , cheerio = require('cheerio')
+
+function getNumber(value) { // returns 0 on invalid input !
+  return Number(value.substr(0, value.indexOf(" ")))
+}
+
+function scrape(roomID) {
+  const options = {
+    url: 'https://www.airbnb.co.uk/rooms/' + roomID + '?s=51',
+    transform: function (body) {
+        return cheerio.load(body);
+    },
+    headers: {
+      'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.94 Safari/537.36'
+    },
+    timeout:15000
+  }
+
+  return request(options)
+    .then($ => {
+      const summary = $('#summary')
+      const mainContent = summary.next()
+      const name = summary.find("div[itemprop='name']").first()
+      const details = summary.find('div')
+        .filter(function(i,el) {
+          return this.children.length == 4
+        }) // Looks like only the details div has 4 children
+        .find('span[aria-hidden!="true"]') // remove the icons
+        .not(':has(span)') // remove the elements holding the icons
+
+      //load JSON data for amenities
+      const bundleJSON = $('script[data-hypernova-key="spaspabundlejs"]').html()
+      const amenities = {}
+      JSON.parse(bundleJSON.substring(4,bundleJSON.length-3))['bootstrapData']['reduxData']['marketplacePdp']['listingInfo']['listing']['listing_amenities'].forEach(i => {
+        amenities[i['tag']] = i['is_present']
+      })
+
+      const property = {
+        name: name.text(),
+        capacity: getNumber(details.eq(0).text()),
+        type: details.eq(1).text(),
+        beds: getNumber(details.eq(2).text()),
+        baths: getNumber(details.eq(3).text()),
+        amenities: amenities
+      }
+
+      return property
+    })
+    .catch(err => {
+      console.log(err)
+      throw 'Scrape failed'
+    })
+}
+
+exports.scrape = scrape
+
